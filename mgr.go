b1964dfafc4573d49067e96ab2ef8f9ecd1f1b39
kyf <kyf456@163.com>
2016-11-10 18:15:15 +0800

    first commit

---
+++ mgr.go
@@ -0,0 +1,76 @@
+package taskmgr
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	DATE_LAYOUT = "2006-01-02 15:04:05"
+)
+
+type Task interface {
+	Do() error
+	String() string
+	Benchmark() string
+}
+
+type Bench struct {
+	ts1 string
+}
+
+func (this *Bench) Begin() {
+	this.ts1 = time.Now().Format(DATE_LAYOUT)
+}
+
+func (this *Bench) Benchmark() string {
+	now := time.Now().Format(DATE_LAYOUT)
+	return fmt.Sprintf("%s~%s", this.ts1, now)
+}
+
+type TaskMgr struct {
+	exit     chan bool
+	monitor  chan<- string
+	taskChan <-chan Task
+	errChan  chan<- string
+}
+
+func NewMgr(monitor, errChan chan<- string, taskChan <-chan Task) *TaskMgr {
+	return &TaskMgr{exit: make(chan bool, 1), monitor: monitor, taskChan: taskChan, errChan: errChan}
+}
+
+func (this *TaskMgr) Run() {
+	for {
+		select {
+		case t := <-this.taskChan:
+			select {
+			case this.monitor <- fmt.Sprintf("task [%s] will handle!", t.String()):
+			default:
+			}
+			go func() {
+				err := t.Do()
+				if err != nil {
+					select {
+					case this.errChan <- fmt.Sprintf("[%s]execute error! reason is %v", t.String(), err):
+					case <-time.After(time.Second * 1):
+					}
+				}
+
+				select {
+				case this.monitor <- fmt.Sprintf("task [%s] benchmark %v!", t.String(), t.Benchmark()):
+				default:
+				}
+			}()
+		case <-this.exit:
+			select {
+			case this.monitor <- "task manager will be exit!":
+			case <-time.After(time.Second * 1):
+			}
+			return
+		}
+	}
+}
+
+func (this *TaskMgr) Stop() {
+	close(this.exit)
+}
---
+++ mgr_test.go
@@ -0,0 +1,54 @@
+package taskmgr
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/kyf/util/log"
+)
+
+type DemoTask struct {
+	Bench
+}
+
+func (this *DemoTask) Do() error {
+	this.Begin()
+	time.Sleep(time.Second * 5)
+	return errors.New("demo testing error")
+}
+
+func (this *DemoTask) String() string {
+	return "DemoTask testing string"
+}
+
+func TestMgr(t *testing.T) {
+	monitor, errChan, taskChan := make(chan string, 1), make(chan string, 1), make(chan Task, 1)
+
+	mgr := NewMgr(monitor, errChan, taskChan)
+	go mgr.Run()
+
+	exit := make(chan bool, 1)
+
+	go func() {
+		for {
+			select {
+			case it := <-monitor:
+				log.Print(it)
+			case it := <-errChan:
+				log.Error(it)
+			case <-exit:
+				return
+			}
+		}
+	}()
+
+	for i := 0; i <= 10; i++ {
+		taskChan <- &DemoTask{}
+	}
+
+	time.Sleep(time.Second * 10)
+	mgr.Stop()
+	time.Sleep(time.Second * 3)
+	close(exit)
+}
